d4eeff96440054d7216c2b7fca4041e4ed404e14
Madjidscript <abdoul.latoundji@uvci.edu.ci>
2024-01-16 14:09:22 +0100

    depot projet

--- public/javascripts/loader.js
+++ public/javascripts/loader.js
@@ -1,18 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Afficher le loader
-    showLoader();
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Afficher le loader
+//     showLoader();
   
-    // Simuler une opération asynchrone (par exemple, une requête AJAX)
-    setTimeout(function() {
-      // Cacher le loader après l'opération
-      hideLoader();
-    }, 2000); // Temps simulé de 3 secondes
-  });
+//     // Simuler une opération asynchrone (par exemple, une requête AJAX)
+//     // setTimeout(function() {
+//     //   // Cacher le loader après l'opération
+//     //   hideLoader();
+//     // }, 2000); // Temps simulé de 3 secondes
+//   });
   
-  function showLoader() {
-    document.getElementById("loaderContainer").style.display = "flex";
-  }
+//   function showLoader() {
+//     document.getElementById("loaderContainer").style.display = "none";
+//   }
   
-  function hideLoader() {
+//   function hideLoader() {
+//     document.getElementById("loaderContainer").style.display = "none";
+//   }
+
+window.onload = function() {
+    // Masquer le loader
     document.getElementById("loaderContainer").style.display = "none";
-  }
+};
