15563a6135552cecdca8bf7343a012af3c5fc5b6
Madjidscript <abdoul.latoundji@uvci.edu.ci>
2024-06-12 00:18:33 +0100

    depot projet

--- public/javascripts/recherche.js
+++ public/javascripts/recherche.js
@@ -1,15 +1,17 @@
 function filterNames() {
     const query = document.getElementById('search-input').value.toLowerCase();
     const body = document.querySelectorAll('.hexagon-item');
+    const box = document.querySelector('.hexagon-menu');
+
 
     body.forEach(item => {
       const name = item.querySelector('.titres').textContent.toLowerCase();
       if (name.includes(query)) {
         item.style.display = '';
       } else {
-        item.textContent= " nous avons pas cette recette";
-        item.style.color = 'red';
-        item.style.fontSize = '30px';
+       box.textContent= " nous avons pas cette recette";
+       box.style.color = 'red';
+       box.style.fontSize = '30px';
         
       }
     });
