315ceaa841fa075c10acf0166e8dac86dd78bdf8
Madjidscript <abdoul.latoundji@uvci.edu.ci>
2025-05-23 12:55:36 +0000

    depot

--- model/modeleCommande.js
+++ model/modeleCommande.js
@@ -3,6 +3,7 @@ const mongoose= require('mongoose')
 const modelSchema = mongoose.Schema({
 num:{type:Number,required:true},
 total:{type:String,required:true},
+index:{type:String,required:true},
 statut: { type: Boolean, default: true },
 data:{type:Array,required:true},
 alergit: {type:Array,default:"pas d'arlergie"}, 
