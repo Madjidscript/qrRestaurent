07daa0485966389e2fa47cfbe8b19765bbed944d
Madjidscript <abdoul.latoundji@uvci.edu.ci>
2025-08-09 23:44:13 +0100

    depot

--- controller/controlerApi.js
+++ controller/controlerApi.js
@@ -1104,7 +1104,7 @@ static recupqr = async (req, res) => {
     });
 
     if (otherTable) {
-      return res.status(403).json({
+      return res.json({
         status:"403",
         message: `Vous êtes déjà connecté à la table ${otherTable.number}. Veuillez patienter ou libérer cette tables.`,
       });
